baa35dce677c57f82c30ad78352575342dcbd066
Motsoeneg <leratok5@gmail.com>
2023-09-30 15:55:26 +0200

    date fully functional

--- src/DateFormat.js
+++ src/DateFormat.js
@@ -1,7 +1,23 @@
 import React from "react";
-
 export default function DateFormat(props) {
   console.log(props.date);
-
-  return "Monday";
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) hours = `01(hours)`;
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) minutes = `01(minutes)`;
+  return (
+    <div>
+      {day} {hours}:{minutes}
+    </div>
+  );
 }
